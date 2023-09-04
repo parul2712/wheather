046db49f7bdc8dad8ad6ca62ae1011e758334e85
parul2712 <143980873+parul2712@users.noreply.github.com>
2023-09-04 10:52:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function fetch_data()
+{
+     var cityname = document.getElementById('city').value
+     console.log(cityname) 
+     if(cityname ==="")
+     {
+        alert('enter city name')
+     }
+     var url ='https://api.openweathermap.org/data/2.5/weather?q=' +cityname+'&appid=eaef821bdf14db8c141254d5a9b21906&units=metric'
+     fetch(url)
+     .then(response => response.json())
+     .then(data => {
+        var resp_code = data['cod']
+        if(resp_code === '404')
+        {
+            alert('city not found')
+        }
+        else
+        {
+            var cityTemp = data['main']['temp']
+            console.log(cityTemp) 
+            var icon = data['weather']['0']['icon']
+            console.log(icon)
+            var icon_url = ' https://openweathermap.org/img/wn/'+icon+'@2x.png'
+            document.getElementById('result').innerHTML=`<img src=`+ icon_url+`>` +`<br>`+`<h3>Temperature is :`+cityTemp+`&deg;c</h3>`
+                }
+     })
+}
+
